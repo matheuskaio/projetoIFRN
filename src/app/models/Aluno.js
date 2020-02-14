14c70e2a05c5db0a8fcbb03a4f5b72dd11fcb528
matheuskaio <m.k.m.p.2000@gmail.com>
2020-02-14 00:22:20 -0300

    Configurando o multer e criando a tabela file

---
+++ src/app/models/Aluno.js
@@ -0,0 +1,23 @@
+// import Sequelize, { Model } from 'sequelize';
+
+// class User extends Model {
+//   static init(sequelize) {
+//     super.init(
+//       {
+//         nome: Sequelize.STRING,
+//         cpf: Sequelize.STRING,
+//         matricula: Sequelize.STRING,
+//         curso: Sequelize.STRING,
+//         curriculoLatte: Sequelize.STRING,
+//         usuario: Sequelize.INTEGER,
+//         foto: Sequelize.INTEGER,
+//       },
+//       {
+//         sequelize,
+//       }
+//     );
+//     return this;
+//   }
+// }
+
+// export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World nada' })
-);
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -15,4 +15,5 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
